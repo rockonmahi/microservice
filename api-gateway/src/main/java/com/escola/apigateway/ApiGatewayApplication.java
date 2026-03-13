ce02e2d11b04ebe9a223022368fe0144c0f76ad5
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-03-13 17:53:17 +0530

    stable code with api-gateway health check only

--- api-gateway/src/main/java/com/escola/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/escola/apigateway/ApiGatewayApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 }
