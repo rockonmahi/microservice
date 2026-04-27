df04f48250c8c2f2aeed42938fa08af04901d37a
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-04-27 19:49:44 +0530

    front-end changes started

--- web-server/src/main.ts
+++ web-server/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { escolaConfig } from './escola/escola.config';
-import {EscolaComponent} from './escola/escola';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
 
-bootstrapApplication(EscolaComponent, escolaConfig)
+registerSwiperElements();
+
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
