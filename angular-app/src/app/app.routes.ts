e37f098b17140b7839f382b68b200c893f98d41f
Mahendra Pratap Singh <34816213+rockonmahi@users.noreply.github.com>
2026-08-20 19:41:00 +0530

    feat: configure Angular authentication routes

---
+++ angular-app/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'auth/callback',
+    loadComponent: () => import('./auth/callback/callback.component').then(m => m.CallbackComponent)
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
